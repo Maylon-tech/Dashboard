9804288a11a2022a992461e3483454dae7f2f70e
Maylon-tech <minatokiroisen77@gmail.com>
2023-12-28 11:24:33 +0900

    first view of dashboard with HTML CSS JS

---
+++ orders.js
@@ -0,0 +1,21 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'pending'
+    },
+    {
+        productName: 'Next.JS Full Course',
+        productNumber: '51043',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'React Native Basic Tutorial',
+        productNumber: '55821',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    
+]
